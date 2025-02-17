ef1aa397fca47f45beff23bbc5296ac7384d817c
Avinashavi4 <167135106+Avinashavi4@users.noreply.github.com>
2025-02-17 12:28:56 -0600

    Initial commit - Fixed line endings

---
+++ src/main/java/net/javaguides/springboot/springboot_crud_tutorial/SpringbootCrudTutorialApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot.springboot_crud_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/javaguides/springboot/springboot_crud_tutorial/SpringbootCrudTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot.springboot_crud_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
